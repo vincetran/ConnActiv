c262f49328c73ad578420c287e68d3d14f864f98
Rob Filippi <Flippi273@gmail.com>
2011-11-04 01:51:03 -0400

    Here are the database files I edited to populate my database. If you import these then you should get no errors when running the php code I last pushed.

---
+++ database/user_networks.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 04, 2011 at 05:47 AM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `xgamings_connactiv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_networks`
+--
+
+CREATE TABLE IF NOT EXISTS `user_networks` (
+  `USER_ID` int(11) NOT NULL,
+  `NETWORK_ID` int(11) NOT NULL,
+  PRIMARY KEY (`USER_ID`,`NETWORK_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_networks`
+--
+
+INSERT INTO `user_networks` (`USER_ID`, `NETWORK_ID`) VALUES
+(1, 0),
+(1, 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
